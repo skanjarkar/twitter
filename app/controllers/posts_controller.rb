f1378f19c3de66924f327cbf6c152b240aa31104
skanjarkar <kanjarkar.sowmya@gmail.com>
2014-10-04 23:03:21 -0400

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+# http://www.noupe.com/development/create-a-simple-twitter-app.html
+class PostsController < ApplicationController
+def index
+ @posts = Post.all(:order=> "created_at DESC")
+ respond_to do |format|
+ 	format.html
+ end
+end
+
+def create
+ @post = Post.create(:message => params[:message])
+ respond_to do |format|
+ 	if @post.save
+ 		format.html {redirect_to posts_path}
+ 		format.js
+ 	else
+ 		flash[:notice] = "Message failed to save"
+ 		format.html {redirect_to posts_path}
+ 	end	
+ end	
+
+end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter::Application.config.secret_token = '90d9fd0dfe08855afa5df95b87aedd1cad90c679d7eb2eaf394b942939b76fdf4919eb4c48c0959aa0f5bb5ae8aa187f0d948ba79b33f66a8ff8308994ec1014'
